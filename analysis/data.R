9ef9b673bd7b088537404cc7c2a6693b14d0b926
erickabsmith <smith.ericka.b@gmail.com>
2021-04-26 20:00:06 -0700

    missed data commit

--- analysis/data.R
+++ analysis/data.R
@@ -1,13 +1,15 @@
 #' title: EBS Continental Shelf Survey Flatfish Visualizations
 #' purpose: This loads all of the cleaned data into the environment.
-#' date: 2020-12-31
+#' date: 2021-04-26
 #' author: Ericka B. Smith
 #' ---
 
+library(readr)
+library(here)
 
-akp <- read_rds(here("data/intermediates", "akp.rds"))
-fhs <- read_rds(here("data/intermediates", "akp.rds"))
-nrs <- read_rds(here("data/intermediates", "akp.rds"))
-yfs <- read_rds(here("data/intermediates", "akp.rds"))
+#akp <- read_rds(here("data/intermediates", "akp.rds"))
+fhs <- read_rds(here("data/intermediates", "fhs.rds"))
+nrs <- read_rds(here("data/intermediates", "nrs.rds"))
+#yfs <- read_rds(here("data/intermediates", "yfs.rds"))
 year_temp_categories <-
   read_rds(here("data/intermediates", "year_temp_categories.rds"))
